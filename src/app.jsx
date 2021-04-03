4d6947466a4d1375c7db4205a75a5c52691f4236
youngjae <kyj@youngjacBookAir.mshome.net>
2021-04-03 22:32:00 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtube}) { //필요한 dependency를 외부로부터 받아와야 한다.
   const [videos, setVideos] = useState([]);
 
   useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBiZX5cTeloahKDGi1o7T_-zcj1MBtcdCk", requestOptions)
-      .then(response => response.json())  //fetch가 받아지면 반응들 json으로 변환하고
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
   }, []);
 
+  const handleSearch = (query) => {
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
+  }
+
   return (
-    <VideoList videos={videos} />
+    <div className={styles.app}>
+      <SearchHeader onSearch={handleSearch}/>
+      <VideoList videos={videos} />
+    </div>
   );
 }
 
